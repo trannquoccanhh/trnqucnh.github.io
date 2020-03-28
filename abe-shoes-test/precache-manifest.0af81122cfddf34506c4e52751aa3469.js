63a48d35a57574a90ccda683279eea16e32878bb
Trần Quốc Anh <goodboygdty@gmail.com>
2020-03-28 18:16:26 +0700

    abe-shoes-test v1.0.0

---
+++ abe-shoes-test/precache-manifest.0af81122cfddf34506c4e52751aa3469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1f54773b9634cb5a56dea63bc3aa71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd5060ee66670615ce0d",
+    "url": "./static/css/main.3f7cf6bb.chunk.css"
+  },
+  {
+    "revision": "e60db59acb545d653e9d",
+    "url": "./static/js/2.b9cbf0ba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.b9cbf0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5060ee66670615ce0d",
+    "url": "./static/js/main.20bca7af.chunk.js"
+  },
+  {
+    "revision": "d62ad5bf391a31873e1e",
+    "url": "./static/js/runtime-main.125a1b0e.js"
+  }
+]);
